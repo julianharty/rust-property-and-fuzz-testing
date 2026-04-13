03d73491e9b4c6a7b48ca873c8c3b45a98ed1c30
julianharty <julianharty@gmail.com>
2026-04-13 09:35:08 +0100

    Added inline property tests, run with `cargo test -- --nocapture to see print output.`
    running 0 tests
    
    test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 1 filtered out; finished in 0.00s
    
    running 0 tests
    
    test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn parse_hex_color(s: &str) -> Option<(u8, u8, u8)> {
+    if s.len() != 7 || !s.starts_with('#') {
+        return None;
+    }
+    let r = u8::from_str_radix(&s[1..3], 16).ok()?;
+    let g = u8::from_str_radix(&s[3..5], 16).ok()?;
+    let b = u8::from_str_radix(&s[5..7], 16).ok()?;
+    Some((r, g, b))
+}
+
+pub fn to_hex_color((r, g, b): (u8, u8, u8)) -> String {
+    format!("#{r:02X}{g:02X}{b:02X}")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use proptest::prelude::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    proptest! {
+        #[test]
+        fn hex_roundtrip(r in any::<u8>(), g in any::<u8>(), b in any::<u8>()) {
+            let s = to_hex_color((r, g, b));
+            println!("{}", s);
+            prop_assert_eq!(parse_hex_color(&s), Some((r, g, b)));
+        }
     }
 }
