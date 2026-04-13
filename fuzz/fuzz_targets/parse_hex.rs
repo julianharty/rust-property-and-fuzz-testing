6daf243eaa45d68e94cdd32c4b13f02866a8f5ff
julianharty <julianharty@gmail.com>
2026-04-13 16:47:16 +0100

    And fixed the fuzz test so it can reference the function it tests.

--- fuzz/fuzz_targets/parse_hex.rs
+++ fuzz/fuzz_targets/parse_hex.rs
@@ -1,9 +1,10 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use your_crate::parse_hex_color;
+use chatgpt_rust_property_and_fuzz_testing::{parse_hex_color};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
+        println!("{}", s);
         let _ = parse_hex_color(s); // should never panic
     }
 });
