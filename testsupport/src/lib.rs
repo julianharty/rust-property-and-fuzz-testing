527d3c71ce71cf1f53c4fd3e1c42515dfac6ecbf
julianharty <julianharty@gmail.com>
2026-04-16 17:58:08 +0100

    Fixed the formatting using .

--- testsupport/src/lib.rs
+++ testsupport/src/lib.rs
@@ -21,9 +21,7 @@ pub fn boundary_breaker_string() -> impl Strategy<Value = String> {
             .filter_map(char::from_u32)
             .collect::<Vec<_>>(),
     );
-    let ascii = prop::sample::select(
-        "0123456789ABCDEFabcdefXYZ ".chars().collect::<Vec<_>>(),
-    );
+    let ascii = prop::sample::select("0123456789ABCDEFabcdefXYZ ".chars().collect::<Vec<_>>());
 
     (ascii.clone(), two_byte.clone(), two_byte, ascii)
         .prop_map(|(a, b, c, d)| format!("#{a}{b}{c}{d}"))
@@ -34,9 +32,23 @@ pub fn hexish_strings() -> impl Strategy<Value = String> {
     let bad = prop::sample::select("GgZz/:-_ ".chars().collect::<Vec<_>>());
 
     prop_oneof![
-        (hex.clone(), hex.clone(), hex.clone(), hex.clone(), hex.clone(), hex.clone())
+        (
+            hex.clone(),
+            hex.clone(),
+            hex.clone(),
+            hex.clone(),
+            hex.clone(),
+            hex.clone()
+        )
             .prop_map(|t| format!("#{}{}{}{}{}{}", t.0, t.1, t.2, t.3, t.4, t.5)),
-        (bad, hex.clone(), hex.clone(), hex.clone(), hex.clone(), hex.clone())
+        (
+            bad,
+            hex.clone(),
+            hex.clone(),
+            hex.clone(),
+            hex.clone(),
+            hex.clone()
+        )
             .prop_map(|t| format!("#{}{}{}{}{}{}", t.0, t.1, t.2, t.3, t.4, t.5)),
         boundary_breaker_string(),
     ]
