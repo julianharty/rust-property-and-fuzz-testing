4430aaf2e8fa7fc730c47731d771cfeb85bd18cd
julianharty <julianharty@gmail.com>
2026-04-16 17:33:32 +0100

    Fixed a compiler warning.

--- fuzz/fuzz_targets/parse_hex.rs
+++ fuzz/fuzz_targets/parse_hex.rs
@@ -7,7 +7,7 @@ use testsupport::HexInput;
 fuzz_target!(|data: &[u8]| {
     let _ = colorlib::parse_hex_color(std::str::from_utf8(data).unwrap_or(""));
 
-    if let Ok(mut u) = Unstructured::new(data).arbitrary::<HexInput>() {
+    if let Ok(u) = Unstructured::new(data).arbitrary::<HexInput>() {
         let s = u.to_ascii_hex_string();
         let _ = colorlib::parse_hex_color(&s);
     }
